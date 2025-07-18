c59757c5acc216278f24fb21efd9bb1eba853a09
mhgolestan <golestan1369@gmail.com>
2025-07-19 01:58:29 +0200

    Rename `genre` field to `genres` in `Movie` model and add factory for testing

--- src/movies/models.py
+++ src/movies/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
-    genre = models.JSONField(default=list)
+    genres = models.JSONField(default=list)
 
     def __str__(self):
         return self.title
