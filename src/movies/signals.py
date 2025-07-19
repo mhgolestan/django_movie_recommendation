27921c77b6da3959b1483dcf4a0e52382666d6f7
mhgolestan <golestan1369@gmail.com>
2025-07-20 00:14:36 +0200

    Add signal to create or update `UserMoviePreference` on `User` save

---
+++ src/movies/signals.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import get_user_model
+from django.db.models.base import ModelBase
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from typing import Any, Type
+
+from movies.models import UserMoviePreference
+
+User = get_user_model()
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_movie_preferences(
+        sender: Type[ModelBase],
+        instance: User,
+        created: bool,
+        **kwargs: Any) -> None:
+    if created:
+        UserMoviePreference.objects.create(user=instance)
+    else:
+        instance.movie_preferences.save()
