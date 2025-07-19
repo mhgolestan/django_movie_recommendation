fa021dfea13b05fd282fdffaeaa3fe7a44b753b8
mhgolestan <golestan1369@gmail.com>
2025-07-19 23:01:33 +0200

    Rename `year` field to `release_year` in `Movie` model and improve data validation; add `country` and `extra_data` fields with supporting migration and tests

--- src/movies/models.py
+++ src/movies/models.py
@@ -1,9 +1,22 @@
+import datetime
+
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     genres = models.JSONField(default=list)
-    year = models.IntegerField(null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    extra_data = models.JSONField(default=dict)
+    release_year = models.IntegerField(
+        validators=[
+            MinValueValidator(1888),
+            MaxValueValidator(datetime.datetime.now().year)
+        ],
+        null=True)
+
+    class Meta:
+        unique_together = ("title", "country", "release_year")
 
     def __str__(self):
         return self.title
--- src/movies/tests/tests_models.py
+++ src/movies/tests/tests_models.py
@@ -4,18 +4,24 @@ from movies.models import Movie, Book
 
 @pytest.mark.django_db
 def test_movie_model():
-    movie = Movie.objects.create(title="A New Hope", genres=["Sci-Fi", "Adventure"], year=1977)
+    movie = Movie.objects.create(
+        title="A New Hope",
+        genres=["Sci-Fi", "Adventure"],
+        release_year=1977
+    )
     assert isinstance(movie, Movie)
     assert movie.title == "A New Hope"
     assert movie.genres == ["Sci-Fi", "Adventure"]
-    assert movie.year == 1977
+    assert movie.release_year == 1977
 
 @pytest.mark.django_db
 def test_book_model():
-    book = Book.objects.create(title="The Great Gatsby",
-                               author="F. Scott Fitzgerald",
-                               isbn="0-6631-2012-8",
-                               publication_year=1925)
+    book = Book.objects.create(
+        title="The Great Gatsby",
+        author="F. Scott Fitzgerald",
+        isbn="0-6631-2012-8",
+        publication_year=1925
+    )
     assert isinstance(book, Book)
     assert book.title == "The Great Gatsby"
     assert book.author == "F. Scott Fitzgerald"
