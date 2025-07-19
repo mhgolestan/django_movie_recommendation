6bb2625ebc782c36de54810761a99c4b29b6d5d0
mhgolestan <golestan1369@gmail.com>
2025-07-19 13:47:02 +0200

    Add `MovieAPIView` to `movies` app for handling CRUD operations

---
+++ src/movies/api.py
@@ -0,0 +1,42 @@
+from rest_framework import views, status
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+
+from movies.models import Movie
+from movies.serializers import MovieSerializer
+
+class MovieAPIView(views.APIView):
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, pk=None):
+        if pk:
+            movie = Movie.objects.get(pk=pk)
+            serializer = MovieSerializer(movie)
+            return Response(serializer.data)
+        else:
+            movies = Movie.objects.all()
+            serializer = MovieSerializer(movies, many=True)
+            return Response({
+                "count": len(serializer.data),
+                "results": serializer.data,
+                "next": None,
+                "previous": None
+            })
+
+    def put(self, request, pk):
+        movie = get_object_or_404(Movie, pk=pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        movie = get_object_or_404(Movie, pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
