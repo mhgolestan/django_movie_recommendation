47f916814adaf97016ccc04ec8f7c376677770da
mhgolestan <golestan1369@gmail.com>
2025-07-19 14:17:27 +0200

    Refactor `MovieAPIView` into `MovieListCreateAPIView` and `MovieDetailAPIView` for improved separation of concerns and use of generic views

--- src/movies/urls.py
+++ src/movies/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from movies.api import MovieAPIView
+from movies.api import MovieListCreateAPIView, MovieDetailAPIView
 
 app_name = "movies"
 
 urlpatterns = [
-    path("movies/", MovieAPIView.as_view(), name="movie-list"),
-    path("movies/<int:pk>/", MovieAPIView.as_view(), name="movie-detail"),
+    path("movies/", MovieListCreateAPIView.as_view(), name="movie-list"),
+    path("movies/<int:pk>/", MovieDetailAPIView.as_view(), name="movie-detail"),
 ]
