00e6de068d604d34ef24da00004cfc5b74014bfc
mhgolestan <golestan1369@gmail.com>
2025-07-19 12:14:15 +0200

    Add api test suite for `movies` app including CRUD and pagination tests

---
+++ src/movies/tests/test_api.py
@@ -0,0 +1,82 @@
+import json
+import pytest
+from django.test import override_settings
+from django.urls import reverse
+from rest_framework import status
+
+from movies.models import Movie
+from .factories import MovieFactory
+
+@pytest.mark.django_db
+def test_create_movie(client):
+    url = reverse('movies:movie-list')
+    data = {"title": "A New Hope",
+            "genres": json.dumps(["Sci-Fi", "Adventure"])}
+
+    response = client.post(url, json=data)
+    assert response.status_code == status.HTTP_201_CREATED, response.json()
+    assert Movie.objects.filter(title="A New Hope") == 1
+
+
+@pytest.mark.django_db
+def test_retrieve_movie(client):
+    movie = MovieFactory()
+    url = reverse('movies:movie-detail', kwargs={'pk': movie.id})
+
+    response = client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "id": movie.id,
+        "title": movie.title,
+        "genres": movie.genres
+    }
+
+@pytest.mark.django_db
+def test_update_movie(client):
+    movie = MovieFactory()
+    new_title = "Updated Movie Title"
+    url = reverse('movies:movie-detail', kwargs={'pk': movie.id})
+    data = {"title": new_title}
+
+    response = client.put(url, data=data, content_type='application/json')
+    assert response.status_code == status.HTTP_200_OK, response.json()
+    movie = Movie.objects.filter(id=movie.id).first()
+    assert movie
+    assert movie.title == new_title
+
+@pytest.mark.django_db
+def test_delete_movie(client):
+    movie = MovieFactory()
+    url = reverse('movies:movie-detail', kwargs={'pk': movie.id})
+    response = client.delete(url)
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert not Movie.objects.filter(id=movie.id).exists()
+
+@pytest.mark.django_db
+@override_settings(REST_FRAMEWORK={'PAGE_SIZE': 10})
+def test_list_movies_with_paginations(client):
+    movies = MovieFactory.create_batch(10)
+    url = reverse('movies:movie-list')
+
+    response = client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert 'count' in data
+    assert 'next' in data
+    assert 'previous' in data
+    assert 'result' in data
+
+    assert data['count'] == 10
+    assert data['next'] is None
+    assert data['previous'] is None
+
+    assert len(data['result']) == 10
+
+    returned_movies_ids = {movie['id'] for movie in data['result']}
+    expected_movies_ids = {movie.id for movie in movies}
+    assert returned_movies_ids == expected_movies_ids
+
+    for movie in data['result']:
+        assert set(movie.keys()) == {'id', 'title', 'genres'}
