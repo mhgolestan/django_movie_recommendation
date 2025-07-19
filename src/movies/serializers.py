39be10cbb2a7c1d04b85f2a515166d147e54df42
mhgolestan <golestan1369@gmail.com>
2025-07-19 14:08:42 +0200

    Refactor `MovieSerializer` to use `ModelSerializer` for simplified implementation

--- src/movies/serializers.py
+++ src/movies/serializers.py
@@ -1,20 +1,8 @@
 from rest_framework import serializers
 from movies.models import Movie
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(label="Movie ID", required=False)
-    title = serializers.CharField(max_length=255)
-    genres = serializers.ListSerializer(
-        child=serializers.CharField(max_length=100),
-        allow_empty=True,
-        default=list
-    )
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ["id", "title", "genres"]
 
-    def create(self, validate_data):
-        return Movie.objects.create(**validate_data)
-
-    def update(self, instance, validate_data):
-        instance.title = validate_data.get('title', instance.title)
-        instance.genres = validate_data.get('genres', instance.genres)
-        instance.save()
-        return instance
