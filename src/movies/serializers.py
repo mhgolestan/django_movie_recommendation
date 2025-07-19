ed7aeb646c5be8d055317d59c36b27f7d8c2533f
mhgolestan <golestan1369@gmail.com>
2025-07-19 21:39:22 +0200

    Add `BookSerializer` and associated tests for validation and instance serialization

--- src/movies/serializers.py
+++ src/movies/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
-from movies.models import Movie
+from movies.models import Movie, Book
+
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ["id", "title", "genres", "year"]
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ["id", "title", "author", "isbn", "publication_year"]
 
 
--- src/movies/tests/test_serializers.py
+++ src/movies/tests/test_serializers.py
@@ -1,7 +1,7 @@
 import pytest
 
-from movies.models import Movie
-from movies.serializers import MovieSerializer
+from movies.models import Movie, Book
+from movies.serializers import MovieSerializer, BookSerializer
 
 @pytest.mark.django_db
 def test_valid_movie_serializer():
@@ -39,4 +39,50 @@ def test_serialize_movie_instance():
         "title": movie.title,
         "genres": movie.genres,
         "year": movie.year
+    }
+
+
+@pytest.mark.django_db
+def test_valid_book_serializer():
+    valid_data = {
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald",
+        "isbn": "0-6631-2012-8",
+        "publication_year": 1925
+    }
+    serializer = BookSerializer(data=valid_data)
+    assert serializer.is_valid()
+    serializer.save()
+
+    assert Book.objects.count() == 1
+    created_book = Book.objects.first()
+    assert created_book.title == valid_data["title"]
+    assert created_book.author == valid_data["author"]
+    assert created_book.isbn == valid_data["isbn"]
+    assert created_book.publication_year == valid_data["publication_year"]
+
+
+@pytest.mark.django_db
+def test_invalid_book_serializer():
+    invalid_data = {
+        "author": "F. Scott Fitzgerald"
+    }
+    serializer = BookSerializer(data=invalid_data)
+    assert not serializer.is_valid()
+    assert "title" in serializer.errors
+
+
+@pytest.mark.django_db
+def test_serialize_book_instance():
+    book = Book.objects.create(title="The Great Gatsby",
+                               author="F. Scott Fitzgerald",
+                               isbn="0-6631-2012-8",
+                               publication_year=1925)
+    serializer = BookSerializer(book)
+    assert serializer.data == {
+        "id": book.id,
+        "title": book.title,
+        "author": book.author,
+        "isbn": book.isbn,
+        "publication_year" : book.publication_year
     }
