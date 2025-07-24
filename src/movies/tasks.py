c69f801e0416aec33787017d4cb25cc0e171e4e0
mhgolestan <golestan1369@gmail.com>
2025-07-24 14:44:35 +0200

    Add Celery `process_file` task with Redis result backend and eager task execution for testing

---
+++ src/movies/tasks.py
@@ -0,0 +1,7 @@
+from celery import Celery, shared_task
+from movies.services import FileProcessor
+
+@shared_task
+def process_file(filename: str, file_type: str) -> int:
+    processor = FileProcessor()
+    return processor.process(filename, file_type)
